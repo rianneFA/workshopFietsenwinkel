df045af7804e241662b951ea8446e5a3fbf3239f
Rianne Smeenk <rianne.smeenk@student.graafschapcollege.nl>
2023-03-17 13:09:39 +0100

    update header and footer

---
+++ header.php
@@ -0,0 +1,25 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>header</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div>
+            <img src="R&RLogoDesign.png" width="160px" height="70px" class="imgLogo">
+            <h2 class="titelH2">De ridder op de witte fiets</h2>
+        </div>
+        
+        <div class="rowButton">
+            <button class="buttonLogin">Login</button>
+            <button class="buttonRegistreer">Registreer</button>
+        </div>
+    </header>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,21 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="slider">
+        <img class="mySlides" src="dames.jpg" alt="">       
+        <img class="mySlides" src="heren.jpg" alt="">
+        <img class="mySlides" src="kinderfiets.jpg" alt="">
+        <img class="mySlides" src="fiets.jpg" alt="">
+    </div>
+
+    <script src="app.js" async></script>
+</body>
+</html>
