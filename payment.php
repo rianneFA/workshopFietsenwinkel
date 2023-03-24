4e3465e0a6819c69f1f09841010c2a04ea251e89
Rianne Smeenk <rianne.smeenk@student.graafschapcollege.nl>
2023-03-24 14:32:12 +0100

    continue payment

--- payment.php
+++ payment.php
@@ -4,7 +4,7 @@
 <html>
     <body>
         <div class="paymentMethod">
-            <div>
+            <div class="leftSide">
                 <div class="adresGegevens">
                     <h2>Adres gegevens</h2>
                     <form action="" method="post">
@@ -24,7 +24,7 @@
                             <label>Postcode</label>
                             <input type="password" name="postcode" class="form-control">
                     </form>
-            </div></div>
+            </div>
 
             <div class="bezorgDatum">
                 <h2>Bezorgdatum</h2>
@@ -34,43 +34,31 @@
                         <input type="date" name="bezorgdatum" class="form-control" value="">
                     </div>   
                 </form>    
-            </div>
+            </div></div>
 
-            <div class="betaalMethodes">
-                <h2>Betaal methodes</h2>
-                
-                <form action="" method="post" class="formPaymethod">
-                    <select name="payMethod" size="5">
-                        <option value="paypal">PayPal</option>
-                        <option value="creditcard">Credit Card</option>
-                        <option value="bank">SEPA/Bank Transfer</option>
-                        <option value="bitcoin">Bitcoin</option>
-                        <option value="achteraf">Achteraf Betalen</option>
-                    </select>
-                    <!-- <div class="paypalPayMethod">
-                        <a class="paymethodA"><image class="paymethodImg" src="img/paypal.png"></image><p class="paymethodP">PayPal</p></a>
-                        <input type="checkbox" name="paypal" class="checkboxPayment" value="">
-                    </div>   
-                    <div class="paypalPayMethod">
-                        <a class="paymethodA"><image class="paymethodImg" src="img/credit-card.png"></image><p class="paymethodP">Credit Card</p></a>
-                        <input type="checkbox" name="creditcard" class="checkboxPayment" value="">
-                    </div> 
-                    <div class="paypalPayMethod">
-                        <a class="paymethodA"><image class="paymethodImg" src="img/bank.png"></image><p class="paymethodP">SEPA/Bank Transfer</p></a>
-                        <input type="" name="paypal" class="checkboxPayment" value="">
-                    </div> 
-                    <div class="paypalPayMethod">
-                        <a class="paymethodA"><image class="paymethodImg" src="img/bitcoin-logo.png"></image><p class="paymethodP">Bitcoin</p></a>
-                        <input type="checkbox" name="paypal" class="checkboxPayment" value="">
-                    </div> 
-                    <div class="paypalPayMethod">
-                        <a class="paymethodA"><image class="paymethodImg" src="img/back-arrow.png"></image><p class="paymethodP">Achteraf Betalen</p></a>
-                        <input type="checkbox" name="paypal" class="checkboxPayment" value="">
-                    </div>  -->
-                </form>    
-            </div>
+            <div class="rightSide">
+                <div class="betaalMethodes">
+                    <h2>Betaal methodes</h2>
+                    <form action="" method="post">
+                        <div class="paypalPayMethod">
+                            <a class="paymethodA"><image class="paymethodImg" src="img/paypal.png"></image><p class="paymethodP">PayPal</p></a>
+                        </div>   
+                        <div class="paypalPayMethod">
+                            <a class="paymethodA"><image class="paymethodImg" src="img/credit-card.png"></image><p class="paymethodP">Credit Card</p></a>
+                        </div> 
+                        <div class="paypalPayMethod">
+                            <a class="paymethodA"><image class="paymethodImg" src="img/bank.png"></image><p class="paymethodP">SEPA/Bank Transfer</p></a>
+                        </div> 
+                        <div class="paypalPayMethod">
+                            <a class="paymethodA"><image class="paymethodImg" src="img/bitcoin-logo.png"></image><p class="paymethodP">Bitcoin</p></a>
+                        </div> 
+                        <div class="paypalPayMethod">
+                            <a class="paymethodA"><image class="paymethodImg" src="img/back-arrow.png"></image><p class="paymethodP">Achteraf Betalen</p></a>
+                        </div> 
+                </div>
 
-            <button>Betalen</button>
+                <button>Betalen</button>
+            </div>    
         </div>    
     </body>
 </html>
