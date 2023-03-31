aa00dcd1544a81fefc3bb3c7199f7bc432b8ecdb
Rianne Smeenk <rianne.smeenk@student.graafschapcollege.nl>
2023-03-31 14:53:31 +0200

    fisnish shop page

---
+++ shop.php
@@ -0,0 +1,25 @@
+<?php
+require_once "header.php";
+?>
+
+<html>
+<body>
+    <div class="shoppage">
+        <h1 class="h1Shop">Shop</h1>
+        <div class="shopOverview">
+            <div class="cardFiets">
+                <!-- <img> -->
+                <div class="imgFiets"></div>
+                <div class="textFiets">
+                    <a class="cardFietsA"><h4 class="merkTypeFiets">Altec Metro - Damesfiets</h4></a>
+                    <h4 class="prijsFiets">€122,60</h4>
+                </div>    
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+<?php
+require_once "footer.php";
+?>
