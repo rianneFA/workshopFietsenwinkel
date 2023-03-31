302db03059f8264a4c582cc009ef60e2ae18741c
Rosalie15V <rosalie.vleeming@student.graafschapcollege.nl>
2023-03-31 14:44:35 +0200

    toggle green color

--- js/payment.js
+++ js/payment.js
@@ -1,34 +1,6 @@
-
-$("#payPalA").on('click', function(){ 
-    color();
-    document.getElementById("payPal").style.color = "#8FEB91";
-})
-
-$('#creditA').on('click', function(){ 
-    color();
-    document.getElementById("credit").style.color = "#8FEB91";
-})
-    
-$('#bankA').on('click', function(){ 
-    color();
-    document.getElementById("bank").style.color = "#8FEB91";
-})
-
-$('#bitcoinA').on('click', function(){ 
-    color();
-    document.getElementById("bitcoin").style.color = "#8FEB91";
-})
-
-$('#achterafA').on('click', function(){ 
-    color();
-    document.getElementById("achteraf").style.color = "#8FEB91";
-})
-
-function color()
-{
-    document.getElementById("payPal").style.color = "black";
-    document.getElementById("credit").style.color = "black";
-    document.getElementById("bank").style.color = "black";
-    document.getElementById("bitcoin").style.color = "black";
-    document.getElementById("achteraf").style.color = "black";
-}
+$(document).ready( () => {
+    $('.paymethodA').on('click', function() {
+        $(this).toggleClass('select');
+        $('.paymethodA').not($(this)).removeClass('select');
+    });
+}); 
